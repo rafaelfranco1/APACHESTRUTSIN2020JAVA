b33851b0e49d480d6a0d0b34ead0c32d633a61b4
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2023-05-23 15:02:41 -0300

    Add files via upload

---
+++ CheckBoxAction.java
@@ -0,0 +1,29 @@
+package br.com.projeto.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@SuppressWarnings("serial")
+public class CheckBoxAction extends ActionSupport{
+	
+	private boolean checkb;
+	
+	
+	public boolean isCheckb() {
+		return checkb;
+	}
+
+
+	public void setCheckb(boolean checkb) {
+		this.checkb = checkb;
+	}
+
+	public String display() {
+		return NONE;
+	}
+
+	public String execute() throws Exception {
+		return "success";
+	}
+	
+	
+}
---
+++ CheckBoxLinkAction.java
@@ -0,0 +1,11 @@
+package br.com.projeto.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@SuppressWarnings("serial")
+public class CheckBoxLinkAction extends ActionSupport{
+	
+	public String execute () throws Exception {
+		return "success";
+	}
+}
