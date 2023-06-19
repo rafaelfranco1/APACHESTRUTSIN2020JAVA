907183630b77b4b26605abf8a96260e1594ed60f
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2023-06-19 18:36:25 -0300

    Update LAction.java
    Generetad Getterss and Setters

--- LAction.java
+++ LAction.java
@@ -1,35 +1,70 @@
-public class LAction extends ActionSupport {
+package br.com.projeto.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@SuppressWarnings("serial")
+public class LAction extends ActionSupport{
 	
-	private List<String> Engine;
-	private List youSearch;
-	private List youMonth;
-	
-	//ggass
-	
-	public String defaultSearch() {
-		return "www.google.com.br";
-	}
-	
-	public LAction() {
 		
-		Engine = new ArrayList<String>();
-		Engine.add("www.google.com.br");
-		Engine.add("www.facebook.com.br");
-		Engine.add("www.java.com");
-		Engine.add("www.youtube.com.br");
-		Engine.add("www.ask.com.br");
-	}
-	
-	
-	public String display() {
+		private List<String> Engine;
+		public List<String> getEngine() {
+			return Engine;
+		}
+
+		public void setEngine(List<String> engine) {
+			Engine = engine;
+		}
+
+		public String getYouSearch() {
+			return youSearch;
+		}
+
+		public void setYouSearch(String youSearch) {
+			this.youSearch = youSearch;
+		}
+
+		public String getYouMonth() {
+			return youMonth;
+		}
+
+		public void setYouMonth(String youMonth) {
+			this.youMonth = youMonth;
+		}
+
+
+		private String youSearch;
+		private String youMonth;
 		
-		return NONE;
-	}
-	
-	
-	public String execute () throws Exception {
-		return "success" 
-	}
-	
-	
-	{
+		
+		
+		public String defaultSearch() {
+			return "www.google.com.br";
+		}
+		
+		public LAction() {
+			
+			Engine = new ArrayList<String>();
+			Engine.add("www.google.com.br");
+			Engine.add("www.facebook.com.br");
+			Engine.add("www.java.com");
+			Engine.add("www.youtube.com.br");
+			Engine.add("www.ask.com.br");
+		}
+		
+		
+		public String display() {
+			
+			return NONE;
+		}
+		
+		
+		public String execute () throws Exception {
+			return "success" 
+		}
+		
+		
+		
+}
