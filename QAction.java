c0404b8808cd92043e28ed7c2625627cffac5131
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2023-06-21 19:25:01 -0300

    Update QAction.java

--- QAction.java
+++ QAction.java
@@ -1,3 +1,11 @@
+package br.com.projeto.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@SuppressWarnings("serial")
 public class QAction extends ActionSupport {
 	
 	private List<String> Engine;
@@ -23,8 +31,32 @@ public class QAction extends ActionSupport {
 		return NONE;
 	}
 	
-	public String execute() throw Exception {
+	public String execute() throws Exception {
 		
 		return "success";
+	}
+
+	public List<String> getEngine() {
+		return Engine;
+	}
+
+	public void setEngine(List<String> engine) {
+		Engine = engine;
+	}
+
+	public String getYouSearch() {
+		return youSearch;
+	}
+
+	public void setYouSearch(String youSearch) {
+		this.youSearch = youSearch;
+	}
+
+	public String getYouMonth() {
+		return youMonth;
+	}
+
+	public void setYouMonth(String youMonth) {
+		this.youMonth = youMonth;
 	}
 }
