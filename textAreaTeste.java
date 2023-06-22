c52fc15f82942ff91f95aac2c414151d28aff54e
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2023-06-22 10:21:36 -0300

    Add files via upload

---
+++ textAreaTeste.java
@@ -0,0 +1,16 @@
+/*
+<li><s:url var="LINK14" action="ta">
+<a href="LINK14">Text Area</a> 
+
+<s:textarea label:"Digite o texto" name="ta" cols="30" row="30"/>
+<s:submit name="submit" key="submit"/>*/
+
+
+
+public class TA extends ActionSupport {
+	
+	private String TA;
+	
+	//ggas Generate Getter and Setters
+}
+
