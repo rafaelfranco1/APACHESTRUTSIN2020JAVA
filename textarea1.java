3e90381d04f2e157e2e61e8f8b5048c5dc9247ed
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2023-06-23 09:04:20 -0300

    Add files via upload

---
+++ textarea1.java
@@ -0,0 +1,35 @@
+/*
+
+<!--Index JSP!-->
+
+<s:url var="LINK14" value="taum"/>
+<a href="%{LINKt14}">TextAreaTest</a>
+
+<!--Taum JSP--!>
+No global properties colocar adress=Endereco
+<s:form action="tadois">
+	<s:textarea label="Adress:" name="adress" col="30" rows="30"/>
+	<s:submit name="submit" key="submit">
+</s:form>
+
+<!--Tadois JSP--!>
+
+<s:property value="adress"/> */
+
+public class TA extends ActionSupport {
+	
+	
+	private String adress;
+	
+	//gggas
+	
+	
+	public String display() {
+		return NONE;
+	}
+	public String execute() {
+		
+		return "success";
+	}
+	
+}
