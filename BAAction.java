4ea3ff6add73813a99fd2722f4b550034e6a9495
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2023-07-04 11:10:45 -0300

    Aula 26 Curso de Java com Struts 2 TextArea TAG

---
+++ BAAction.java
@@ -0,0 +1,29 @@
+/*<!--Index.jsp!-->
+<s:url var="{LINKX}" value="baum"/> <!--seria Taum a primeira página do TextArea . TA significa 
+text area . Logo como já existe TaUM (TextaREA UM) agora é BAUM (Bext Area um)-->
+<a href="%{LINKX}">TextArea</a>
+
+<!--baum-->
+
+<s:form action = "badois"/>
+	<s:textarea name="address" cols="30" rows="30"/> <!--adress deve existir no Properties!-->
+</s:form>
+
+<!--badois-->
+
+<s:property value="address"/>*/
+
+public class BAAction extends ActionSupport {
+	
+	public String address;
+	
+	//ggas
+	
+	public String execute() throws Exception {
+		return "success";
+	}
+	
+	public String display () {
+		return NONE;
+}
+
