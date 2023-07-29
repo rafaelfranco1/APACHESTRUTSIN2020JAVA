03df117ae888d19de195cecb60e1ccbbeb11eced
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2023-07-29 10:12:46 -0300

    Add files via upload

---
+++ SAction.java
@@ -0,0 +1,62 @@
+package br.com.projeto.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@SuppressWarnings("serial")
+public class SAction extends ActionSupport{
+	
+	private List<String> Engine;
+	private String youMonth;
+	private String youSearch;
+	
+	
+	
+	public List<String> getEngine() {
+		return Engine;
+	}
+
+	public void setEngine(List<String> engine) {
+		Engine = engine;
+	}
+
+	public String getYouMonth() {
+		return youMonth;
+	}
+
+	public void setYouMonth(String youMonth) {
+		this.youMonth = youMonth;
+	}
+
+	public String getYouSearch() {
+		return youSearch;
+	}
+
+	public void setYouSearch(String youSearch) {
+		this.youSearch = youSearch;
+	}
+	
+	public String getDefaultSearch() {
+		return "www.google.com.br";
+	}
+	
+	public SAction() {
+		Engine = new ArrayList<String>();
+		Engine.add("www.youtube.com.br");
+		Engine.add("www.google.com.br");
+		Engine.add("www.facebook.com.br");
+		Engine.add("www.twitter.com.br");
+		Engine.add("www.java.com");
+	}
+	
+
+	public String display() {
+		return "NONE";
+	}
+	
+	public String execute() throws Exception {
+		return "success";
+	}
+}
