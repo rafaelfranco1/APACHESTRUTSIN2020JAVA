f4f75452be0bdd7f5915f3347a10e567443a0bdb
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2024-03-27 18:54:26 -0300

    Add files via upload

---
+++ shakann.java
@@ -0,0 +1,17 @@
+private String Fruit1;
+private String Fruit2;
+private String Server1;
+private String Server2;
+private String Language1;
+private String Language2;
+//ggas
+Map languageMap; //importar
+
+public DoubleSelectAction() {
+	
+	languageMap = new HashMap(); //ver o que diabos é HashMap
+	languageMap.put("SQL",new ArrayList<String>(Arrays.asList("MS","Oracle"));
+	languageMap.put("Oracle",new ArrayList<String>(Arrays.asList("11g","10g","9g"));
+	languageMap.put("PostGresql",new ArrayList<String>(Arrays.asList("SQL Postgrees"));
+		
+}
