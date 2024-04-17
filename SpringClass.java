de085f17754ed22361fc0ffaf45c076344f5a035
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2024-04-17 08:49:39 -0300

    Add files via upload

---
+++ SpringClass.java
@@ -0,0 +1,11 @@
+private class Club {
+	
+	private Long id;
+	private String title;
+	private String photoUrl;
+	private String content;
+	private LocalDateTime createDn;
+	private LocalDateTime updateDn;
+
+
+}
