1722c95d257a8816acf1dc95720512da2e0bb2ea
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2024-07-01 12:57:55 -0300

    Update postman.java

--- postman.java
+++ postman.java
@@ -13,11 +13,44 @@ public class ClienteController {
 	
 	//verbo http para chamar o método
 	@GetMapping
+	/*
 	public String hello() {
 		return "Olá, mundo";
+	}*/
+	
+    public List<Client> listar() {
+		
+		
 	}
 	
 	
-	
-	
 }
+
+// classe de cliente
+@Entity //javax.persistence vai existir uma tabela cliente no bd
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)  
+	private String nome;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public Long setId (Long id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String setNome(String nome) {
+		this.nome = nome;
+	}
+}
+
+//SPring Data JPA -> Repositórios para java 
