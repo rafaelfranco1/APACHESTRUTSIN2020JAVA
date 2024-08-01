3124751c4f6da5c7a43fecd30ffbc8545b5ee82b
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2024-08-01 08:50:20 -0300

    Add files via upload

---
+++ RaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RunGroopWebApp1.Controllers
+{
+    public class RaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
