0ec9a933c7e098054130920579354f7038ec353e
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2024-08-05 09:01:53 -0300

    Add files via upload

---
+++ dbcontext.cs
@@ -0,0 +1,17 @@
+public class ClubController : Controller
+{
+
+	private readonly ApplicationDbContext _context;
+	
+	public ClubController(ApplicationDbContext context)
+	{
+		_context = context;
+	}
+	
+	public IActionResult Index() 
+	{
+		
+		var clubs = _context.Clubs.ToList();
+		return View(clubs);
+	}
+}
