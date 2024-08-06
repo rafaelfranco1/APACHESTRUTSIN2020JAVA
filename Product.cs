138ea40385fffb425212c5163ad296d50fa7a321
rafaelfranco1 <42389814+rafaelfranco1@users.noreply.github.com>
2024-08-06 14:34:17 -0300

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BestStoreMVC.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; } = "";
+        [MaxLength(100)]
+        public string Brand { get; set; } = "";
+        [MaxLength(100)]
+        public string Category { get; set; } = "";
+        [Precision (16,2)]
+        public string Price { get; set; } = "";
+
+        public string Description { get; set; } = "";
+        [MaxLength(100)]
+        public string ImageFileName { get; set } = "";
+
+        public DateTime CreatedAt { get; set; } 
+
+    }
+}
